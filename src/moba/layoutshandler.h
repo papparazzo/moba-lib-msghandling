b51125650bd9d7435d582ecf66530c46b848d6fc
papparazzo <pappi-@gmx.de>
2019-02-12 11:12:03 +0100

    layoutshandler implemented

--- src/moba/layoutshandler.h
+++ src/moba/layoutshandler.h
@@ -21,38 +21,153 @@
 #pragma once
 
 #include <string>
+#include <vector>
 
-#include "jsonabstractitem.h"
+#include <moba/jsonabstractitem.h>
+#include "shared.h"
 
+struct LayoutsGetLayoutsReq : public DispatchMessageType<LayoutsGetLayoutsReq> {
+    static std::string getMessageName() {
+        return "LAYOUTS_GET_LAYOUTS_REQ";
+    }
+};
 
-/*
-
-            void sendGetLayoutsRequest() {msgep->sendMsg(Message::MT_GET_LAYOUTS_REQ);}
-
-            void sendDeleteLayout(long id) {msgep->sendMsg(Message::MT_DEL_LAYOUT, toJsonNumberPtr(id));}
-
-            void sendCreateLayout(const std::string &name, const std::string &description);
-
-            void sendUpdateLayout(long id, const std::string &name, const std::string &description);
-
-            void sendUnlockLayout(long id) {msgep->sendMsg(Message::MT_UNLOCK_LAYOUT, toJsonNumberPtr(id));}
-
-            void sendGetLayoutRequest(long id) {msgep->sendMsg(Message::MT_GET_LAYOUT_REQ, toJsonNumberPtr(id));}
-
-
-
-    void MsgLayoutsHandler::sendCreateLayout(const std::string &name, const std::string &description) {
-        JsonObjectPtr obj(new JsonObject());
-        (*obj)["name"       ] = toJsonStringPtr(name);
-        (*obj)["description"] = toJsonStringPtr(description);
-        msgep->sendMsg(Message(Message::MT_CREATE_LAYOUT_REQ, obj));
+struct LayoutsGetLayoutsRes : public RecieveMessage {
+    LayoutsGetLayoutsRes(moba::JsonItemPtr data) {
+        auto a = boost::dynamic_pointer_cast<moba::JsonArray>(data);
+        for(auto iter : *a) {
+            layouts.push_back(TrackLayoutData{boost::dynamic_pointer_cast<moba::JsonObject>(iter)});
+        }
     }
 
-    void MsgLayoutsHandler::sendUpdateLayout(long id, const std::string &name, const std::string &description) {
-        JsonObjectPtr obj(new JsonObject());
-        (*obj)["id"         ] = toJsonNumberPtr(id);
-        (*obj)["name"       ] = toJsonStringPtr(name);
-        (*obj)["description"] = toJsonStringPtr(description);
-        msgep->sendMsg(Message(Message::MT_UPDATE_LAYOUT, obj));
+    static std::string getMessageName() {
+        return "LAYOUTS_GET_LAYOUTS_RES";
     }
-*/
+
+    std::vector<TrackLayoutData> layouts;
+};
+
+struct LayoutsDeleteLayout : public DispatchMessageType<LayoutsDeleteLayout> {
+    LayoutsDeleteLayout(int layoutId) : layoutId{layoutId} {
+    }
+
+    static std::string getMessageName() {
+        return "LAYOUTS_DELETE_LAYOUT";
+    }
+
+    virtual moba::JsonItemPtr getData() const override {
+        return moba::toJsonNumberPtr(layoutId);
+    }
+
+    int layoutId;
+};
+
+struct LayoutsLayoutDeleted : public RecieveMessage {
+    LayoutsLayoutDeleted(moba::JsonItemPtr data) {
+        layoutId = moba::castToInt(data);
+    }
+
+    static std::string getMessageName() {
+        return "LAYOUTS_LAYOUT_DELETED";
+    }
+
+    int layoutId;
+};
+
+struct LayoutsCreateLayoutReq : public DispatchMessageType<LayoutsCreateLayoutReq> {
+    LayoutsCreateLayoutReq(const TrackLayoutData &tracklayout) : tracklayout{tracklayout} {
+    }
+
+    static std::string getMessageName() {
+        return "LAYOUTS_CREATE_LAYOUT_REQ";
+    }
+
+    virtual moba::JsonItemPtr getData() const override {
+        moba::JsonObjectPtr obj(new moba::JsonObject());
+        (*obj)["name"       ] = moba::toJsonStringPtr(tracklayout.name);
+        (*obj)["description"] = moba::toJsonStringPtr(tracklayout.description);
+        return obj;
+    }
+
+    TrackLayoutData tracklayout;
+};
+
+struct LayoutsCreateLayoutRes : public RecieveMessage {
+    LayoutsCreateLayoutRes(moba::JsonItemPtr data) {
+        layoutId = moba::castToInt(data);
+    }
+
+    static std::string getMessageName() {
+        return "LAYOUTS_CREATE_LAYOUT_RES";
+    }
+
+    int layoutId;
+};
+
+struct LayoutsLayoutCreated : public RecieveMessage {
+    LayoutsLayoutCreated(moba::JsonItemPtr data) : tracklayout{boost::dynamic_pointer_cast<moba::JsonObject>(data)} {
+    }
+
+    static std::string getMessageName() {
+        return "LAYOUTS_LAYOUT_CREATED";
+    }
+
+    TrackLayoutData tracklayout;
+};
+
+struct LayoutsUpdateLayout : public DispatchMessageType<LayoutsUpdateLayout> {
+    LayoutsUpdateLayout(const TrackLayoutData &tracklayout) : tracklayout{tracklayout} {
+    }
+
+    static std::string getMessageName() {
+        return "LAYOUTS_UPDATE_LAYOUT";
+    }
+
+    virtual moba::JsonItemPtr getData() const override {
+        moba::JsonObjectPtr obj(new moba::JsonObject());
+        (*obj)["id"         ] = moba::toJsonNumberPtr(tracklayout.id);
+        (*obj)["name"       ] = moba::toJsonStringPtr(tracklayout.name);
+        (*obj)["description"] = moba::toJsonStringPtr(tracklayout.description);
+        return obj;
+    }
+
+    TrackLayoutData tracklayout;
+};
+
+struct LayoutsLayoutUpdated : public RecieveMessage {
+    LayoutsLayoutUpdated(moba::JsonItemPtr data) : tracklayout{boost::dynamic_pointer_cast<moba::JsonObject>(data)} {
+    }
+
+    static std::string getMessageName() {
+        return "LAYOUTS_LAYOUT_UPDATED";
+    }
+
+    TrackLayoutData tracklayout;
+};
+
+struct LayoutsUnlockLayout : public DispatchMessageType<LayoutsUnlockLayout> {
+    LayoutsUnlockLayout(int layoutId) : layoutId{layoutId} {
+    }
+
+    static std::string getMessageName() {
+        return "LAYOUTS_UNLOCK_LAYOUT";
+    }
+
+    virtual moba::JsonItemPtr getData() const override {
+        return moba::toJsonNumberPtr(layoutId);
+    }
+
+    int layoutId;
+};
+
+struct LayoutsLayoutUnlocked : public RecieveMessage {
+    LayoutsLayoutUnlocked(moba::JsonItemPtr data) {
+        layoutId = moba::castToInt(data);
+    }
+
+    static std::string getMessageName() {
+        return "LAYOUTS_LAYOUT_UNLOCKED";
+    }
+
+    int layoutId;
+};
