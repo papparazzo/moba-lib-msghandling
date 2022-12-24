9ca4de0f205eed0169d4b69b6d870a29e9bd5c27
papparazzo <pappi-@gmx.de>
2022-12-25 00:49:21 +0100

    timermessage refactored

--- src/moba/timermessages.h
+++ src/moba/timermessages.h
@@ -28,15 +28,13 @@
 #include "message.h"
 #include "shared.h"
 #include "day.h"
+#include "timestruct.h"
 
 struct TimerMessage : public Message {
     enum MessageName {
         TIMER_GLOBAL_TIMER_EVENT = 1,
         TIMER_GET_GLOBAL_TIMER   = 2,
-        TIMER_SET_GLOBAL_TIMER   = 3,
-        TIMER_GET_COLOR_THEME    = 4,
-        TIMER_SET_COLOR_THEME    = 5,
-        TIMER_COLOR_THEME_EVENT  = 6
+        TIMER_SET_GLOBAL_TIMER   = 3
     };
 
     static constexpr std::uint32_t GROUP_ID = TIMER;
@@ -46,21 +44,12 @@ struct TimerGlobalTimerEvent : public TimerMessage {
     static constexpr std::uint32_t MESSAGE_ID = TIMER_GLOBAL_TIMER_EVENT;
 
     TimerGlobalTimerEvent(const rapidjson::Document &d) {
-        curModelDay = stringToDayEnum(d["curModelDay"].GetString());
-        auto tmp = std::string(d["curModelTime"].GetString());
-        if(tmp.size() != 5) {
-            throw moba::UnsupportedOperationException{"invalid time given"};
-        }
-        hours = std::stoi(tmp.substr(0, 2));
-        minutes = std::stoi(tmp.substr(3, 2));
-        multiplicator = d["multiplicator"].GetInt();
+        curModelDay = stringToDayEnum(d["day"].GetString());
+        time = Time{d["time"].GetUint()};
     }
 
-    unsigned int minutes;
-    unsigned int hours;
-
+    Time time;
     Day curModelDay;
-    unsigned int multiplicator;
 };
 
 struct TimerGetGlobalTimer : public TimerMessage {
@@ -70,90 +59,54 @@ struct TimerGetGlobalTimer : public TimerMessage {
 struct TimerSetGlobalTimer : public TimerMessage {
     static constexpr std::uint32_t MESSAGE_ID = TIMER_SET_GLOBAL_TIMER;
 
-    TimerSetGlobalTimer(Day curModelDay, const std::string &curModelTime, unsigned int multiplicator) :
-    curModelDay{curModelDay}, curModelTime{curModelTime}, multiplicator{multiplicator} {
+    TimerSetGlobalTimer(
+        Day curModelDay, Time curModelTime, unsigned int multiplicator,
+        Time sunriseStartTime, Time dayStartTime, Time sunsetStartTime, Time nightStartTime
+    ):
+        curModelDay{curModelDay}, curModelTime{curModelTime}, multiplicator{multiplicator},
+        nightStartTime{nightStartTime}, sunriseStartTime{sunriseStartTime}, dayStartTime{dayStartTime},
+        sunsetStartTime{sunsetStartTime}
+    {
     }
 
     TimerSetGlobalTimer(const rapidjson::Document &d) {
-        curModelDay = stringToDayEnum(d["curModelDay"].GetString());
-        curModelTime = d["curModelTime"].GetString();
+        auto modelTime = d["modelTime"].GetObject();
+        curModelDay = stringToDayEnum(modelTime["day"].GetString());
+        curModelTime = Time{modelTime["time"].GetUint()};
         multiplicator = d["multiplicator"].GetInt();
-        if(curModelTime.size() != 5) {
-            throw moba::UnsupportedOperationException{"invalid time given"};
-        }
-        hours = std::stoi(curModelTime.substr(0, 2));
-        minutes = std::stoi(curModelTime.substr(3, 2));
+
+        nightStartTime = Time{d["nightStartTime"].GetUint()};
+        sunriseStartTime = Time{d["sunriseStartTime"].GetUint()};
+        dayStartTime = Time{d["dayStartTime"].GetUint()};
+        sunsetStartTime = Time{d["sunsetStartTime"].GetUint()};
     }
 
     rapidjson::Document getJsonDocument() const override {
         rapidjson::Document d;
         d.SetObject();
+
+        rapidjson::Value v;
+        v.SetObject();
+
         auto tmp = dayEnumToString(curModelDay);
-        d.AddMember("curModelDay", rapidjson::Value(tmp.c_str(), tmp.length(), d.GetAllocator()), d.GetAllocator());
-        d.AddMember("curModelTime", rapidjson::Value(curModelTime.c_str(), curModelTime.length(), d.GetAllocator()), d.GetAllocator());
+        v.AddMember("day", rapidjson::Value(tmp.c_str(), tmp.length(), d.GetAllocator()), d.GetAllocator());
+        v.AddMember("time", curModelTime.getTime(), d.GetAllocator());
+
+        d.AddMember("modelTime", v, d.GetAllocator());
         d.AddMember("multiplicator", multiplicator, d.GetAllocator());
+        d.AddMember("nightStartTime", nightStartTime.getTime(), d.GetAllocator());
+        d.AddMember("sunriseStartTime", sunriseStartTime.getTime(), d.GetAllocator());
+        d.AddMember("dayStartTime", dayStartTime.getTime(), d.GetAllocator());
+        d.AddMember("sunsetStartTime", sunsetStartTime.getTime(), d.GetAllocator());
+
         return d;
     }
-
-    unsigned int minutes;
-    unsigned int hours;
 
     Day curModelDay;
-    std::string curModelTime;
+    Time curModelTime;
     unsigned int multiplicator;
-};
-
-struct TimerGetColorTheme : public TimerMessage {
-    static constexpr std::uint32_t MESSAGE_ID = TIMER_GET_COLOR_THEME;
-};
-
-struct TimerSetColorTheme : public TimerMessage {
-    static constexpr std::uint32_t MESSAGE_ID = TIMER_SET_COLOR_THEME;
-
-    TimerSetColorTheme(const std::string &dimTime, const std::string &brightTime, ThreeState condition) :
-    dimTime{dimTime}, brightTime{brightTime}, condition{condition} {
-    }
-
-    TimerSetColorTheme(const rapidjson::Document &d) {
-        dimTime = d["dimTime"].GetString();
-        brightTime = d["brightTime"].GetInt();
-        condition = stringToThreeStateEnum(d["condition"].GetString());
-    }
-
-    rapidjson::Document getJsonDocument() const override {
-        rapidjson::Document d;
-        d.SetObject();
-
-        auto conditionStr = threeStateEnumToString(condition);
-        d.AddMember("dimTime", rapidjson::Value(dimTime.c_str(), dimTime.length(), d.GetAllocator()), d.GetAllocator());
-        d.AddMember("brightTime", rapidjson::Value(brightTime.c_str(), brightTime.length(), d.GetAllocator()), d.GetAllocator());
-        d.AddMember("condition", rapidjson::Value(conditionStr.c_str(), conditionStr.length(), d.GetAllocator()), d.GetAllocator());
-        return d;
-    }
-
-    std::string dimTime;
-    std::string brightTime;
-    ThreeState condition;
-};
-
-struct TimerColorThemeEvent : public TimerMessage {
-    static constexpr std::uint32_t MESSAGE_ID = TIMER_COLOR_THEME_EVENT;
-
-    enum class ColorTheme {
-		BRIGHT,
-		DIM
-    };
-
-    TimerColorThemeEvent(const rapidjson::Document &d) {
-        std::string status = d["dimTime"].GetString();
-        if(status == "BRIGHT") {
-            colorTheme = ColorTheme::BRIGHT;
-        } else if(status == "DIM") {
-            colorTheme = ColorTheme::DIM;
-        } else {
-            throw moba::UnsupportedOperationException{"invalid value given"};
-        }
-    }
-
-    ColorTheme colorTheme;
+    Time nightStartTime;
+    Time sunriseStartTime;
+    Time dayStartTime;
+    Time sunsetStartTime;
 };
