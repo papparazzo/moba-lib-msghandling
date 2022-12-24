9356b7c675742a0661c67811e4aff2fbe84ab66b
papparazzo <pappi-@gmx.de>
2022-12-25 00:49:02 +0100

    timestruct added

---
+++ src/moba/timestruct.cpp
@@ -0,0 +1,97 @@
+/*
+ *  Project:    moba-systemmanager
+ *
+ *  Copyright (C) 2022 Stefan Paproth <pappi-@gmx.de>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/agpl.txt>.
+ *
+ */
+
+#include "timestruct.h"
+#include <sstream>
+
+Time::Time(): time{0} {
+}
+
+Time::Time(unsigned int hours, unsigned int minutes) {
+    time = hours * 60 + minutes;
+}
+
+Time::Time(unsigned int time): time{time} {
+}
+
+Time::Time(const std::string &str) {
+    if(str.length() != 5) {
+        time = 0;
+        return;
+    }
+
+    time = std::stoi(str.substr(0, 2)) * 60 + std::stoi(str.substr(3, 2));
+}
+
+unsigned int Time::getHours() const {
+    return time / 60;
+}
+
+unsigned int Time::getMinutes() const {
+    return time % 60;
+}
+
+unsigned int Time::getTime() const {
+    return time;
+}
+
+std::string Time::getTimeAsString() const {
+
+    auto h = getHours();
+    auto m = getMinutes();
+
+    std::stringstream ss;
+
+    if(h < 10) {
+        ss << "0";
+    }
+    ss << h << ":";
+
+    if(m < 10) {
+        ss << "0";
+    }
+    ss << m;
+    return ss.str();
+}
+
+bool operator== (const Time& t1, const Time& t2) {
+    return t1.time == t2.time;
+}
+
+bool operator!= (const Time& t1, const Time& t2) {
+    return t1.time != t2.time;
+}
+
+bool operator< (const Time& t1, const Time& t2) {
+    return t1.time < t2.time;
+}
+
+bool operator> (const Time& t1, const Time& t2) {
+    return t1.time > t2.time;
+}
+
+bool operator<= (const Time& t1, const Time& t2) {
+    return t1.time <= t2.time;
+}
+
+bool operator>= (const Time& t1, const Time& t2) {
+    return t1.time >= t2.time;
+}
+
---
+++ src/moba/timestruct.h
@@ -0,0 +1,51 @@
+/*
+ *  Project:    moba-systemmanager
+ *
+ *  Copyright (C) 2022 Stefan Paproth <pappi-@gmx.de>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/agpl.txt>.
+ *
+ */
+
+#pragma once
+
+#include <string>
+
+struct Time final {
+    Time();
+    Time(unsigned int hours, unsigned int minutes);
+    Time(unsigned int time);
+    Time(const std::string &time);
+    Time(const Time& t) = default;
+
+    unsigned int getHours() const;
+    unsigned int getMinutes() const;
+    unsigned int getTime() const;
+    std::string getTimeAsString() const;
+
+    friend bool operator== (const Time& t1, const Time& t2);
+    friend bool operator!= (const Time& t1, const Time& t2);
+
+    friend bool operator< (const Time& t1, const Time& t2);
+    friend bool operator> (const Time& t1, const Time& t2);
+
+    friend bool operator<= (const Time& t1, const Time& t2);
+    friend bool operator>= (const Time& t1, const Time& t2);
+
+
+
+private:
+    unsigned int time;
+
+};
