515bc122ce4fe8aa6c9968044ba6c9a615ee01be
papparazzo <pappi-@gmx.de>
2023-08-27 11:08:05 +0200

    header bugfix

--- src/moba/endpoint.h
+++ src/moba/endpoint.h
@@ -44,6 +44,7 @@ public:
 
     auto receiveMsg(time_t timeoutSec = 0) -> RawMessage;
     auto waitForNewMsg() -> RawMessage;
+    std::string waitForNewMsgAsString();
 
     template<typename T>
     void sendMsg(const T &msg) {
