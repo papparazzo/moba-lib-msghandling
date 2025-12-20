5705af13d968c7c68f04cbd39ac6e02bdf590af1
papparazzo <pappi-@gmx.de>
2025-12-20 15:19:07 +0100

    Remove unused CLIENT_SHUTDOWN message and adjust enum values.

--- src/moba/clientmessages.h
+++ src/moba/clientmessages.h
@@ -33,10 +33,9 @@ struct ClientMessage: Message {
         CLIENT_ERROR        = 4,
         CLIENT_START        = 5,
         CLIENT_CONNECTED    = 6,
-        CLIENT_SHUTDOWN     = 7,
-        CLIENT_RESET        = 8,
-        CLIENT_SELF_TESTING = 9,
-        CLIENT_CLOSING      = 10
+        CLIENT_RESET        = 7,
+        CLIENT_SELF_TESTING = 8,
+        CLIENT_CLOSING      = 9
     };
 
     static constexpr std::uint32_t GROUP_ID = CLIENT;
@@ -124,13 +123,6 @@ struct ClientConnected final: ClientMessage {
     }
 
     long appId;
-};
-
-struct ClientShutdown final: ClientMessage {
-    static constexpr std::uint32_t MESSAGE_ID = CLIENT_SHUTDOWN;
-
-    explicit ClientShutdown(const nlohmann::json &) {
-    }
 };
 
 struct ClientReset final: ClientMessage {
